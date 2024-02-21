736cfe749013b120a48d048fbabdaafe762564bc
Leenard <lolleenard0@gmail.com>
2024-02-21 18:45:16 +0800

    feat: removed ts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/") {
-    // res.statusCode(200);
     res.end();
   }
 });
