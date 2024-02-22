41ce3dd0afa9be36b6a84fb7c3c7f076f343edd6
Leenard <lolleenard0@gmail.com>
2024-02-22 13:04:53 +0800

    feat: router module imported

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
-import * as routes from "./routes";
+import { product_router, rn_router, update_router } from "./routes";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200);
 });
+
+app.use("/api", product_router);
+app.use("/api", rn_router);
+app.use("/api", update_router);
 
 export default app;
