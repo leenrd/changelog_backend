b4bc6c295ebe8efc575954ee64ca39dcd5bdf108
Leenard <lolleenard0@gmail.com>
2024-02-22 14:37:23 +0800

    feat: middlewares init

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from "express";
 import { product_router, rn_router, update_router } from "./routes";
+import morgan from "morgan";
+import cors from "cors";
+
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200);
