3b2808047e0801d501d63adb07f6eb18dbb3daf5
Leenard <lolleenard0@gmail.com>
2024-02-23 15:47:38 +0800

    feat: validator middleware

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,13 +1,30 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { errorHandler } from "../utils/validatorMiddleware";
+import { error } from "console";
 
 const product_router = Router();
 
 product_router.get("/product", (req, res) => {
   res.json({ message: "Hello World" });
 });
-product_router.get("/product/:id", () => {});
-product_router.put("/product/:id", () => {});
+
+product_router.get(
+  "/product/:id",
+  body("name").isString(),
+  errorHandler,
+  (req, res) => {}
+);
+
+product_router.put(
+  "/product/:id",
+  body("name").isString(),
+  errorHandler,
+  (req, res) => {}
+);
+
 product_router.delete("/product/:id", () => {});
+
 product_router.post("/product", () => {});
 
 export default product_router;
